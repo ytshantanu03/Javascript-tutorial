07e41a9d3efad11be62356781bbaae615ed46365
ytshantanu03 <ytshantanu1309@gmail.com>
2025-02-22 18:00:34 +0530

    3rd commit

---
+++ Basics/conversionOperations.js
@@ -0,0 +1,30 @@
+let score = "33";
+// let score = "33asd";
+// let score = null;
+// let score = undefined;
+// let score = true;
+
+// console.log(typeof score);
+// console.log(score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33asd" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "naruto" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
