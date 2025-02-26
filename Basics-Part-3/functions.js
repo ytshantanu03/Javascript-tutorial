07c2f3b717a4175905f85b68993cf25143256a9f
ytshantanu03 <ytshantanu1309@gmail.com>
2025-02-26 08:47:26 +0530

    15th commit

---
+++ Basics-Part-3/functions.js
@@ -0,0 +1,41 @@
+function sayMyName() {
+  console.log("N");
+  console.log("A");
+  console.log("R");
+  console.log("U");
+  console.log("T");
+  console.log("O");
+}
+// sayMyName();
+
+// function addTwoNumbers(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+function addTwoNumbers(num1, num2) {
+  //   let result = num1 + num2;
+  //   return result;
+
+  return num1 + num2;
+
+  //   console.log("Naruto"); // anything after return will not execute
+}
+
+const result = addTwoNumbers(3, 5);
+// console.log("Result: ", result);
+
+function loginUserMessage(username) {
+  //   if (username === undefined) {
+  //     console.log("Please enter a username");
+  //     return;
+  //   }
+
+  if (!username) {
+    console.log("Please enter a username");
+    return;
+  }
+
+  return `${username} just logged in`;
+}
+// console.log(loginUserMessage("Naruto"));
+console.log(loginUserMessage());
