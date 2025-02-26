650c4f03a6b3f1fa2f27b6b6a8a626f4e33386ea
ytshantanu03 <ytshantanu1309@gmail.com>
2025-02-26 09:23:14 +0530

    16th commit

--- Basics-Part-3/functions.js
+++ Basics-Part-3/functions.js
@@ -38,4 +38,32 @@ function loginUserMessage(username) {
   return `${username} just logged in`;
 }
 // console.log(loginUserMessage("Naruto"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(...num1) {
+  return num1;
+}
+console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+  username: "Naruto",
+  price: 199,
+};
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+// handleObject(user);
+handleObject({
+  username: "sam",
+  price: 399,
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
